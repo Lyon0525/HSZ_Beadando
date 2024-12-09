f012c67d82e2b2bd5f10850b350601c29cfded92
Lyon0525 <palfikristof2004.kfpl@gmail.com>
2024-12-09 19:19:27 +0100

    Linq

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,7 @@ namespace HSZ_Beadando {
                 Random r = new Random();
                 reactor = new Reaktor(r.Next(5, 11), r.Next(4, 6), r.Next(550, 651));
                 for (int i = 0; i < 24; i++) { //1 perc
+                    //Delegált: Zoli
                     if (i % 3 == 0) {
                         reactor.Delegalt(true);
                     }
@@ -24,15 +26,32 @@ namespace HSZ_Beadando {
                     }
                     reactor.Kiiras();
                 }
-                //Delegált: Zoli
-
                 //Linq: Zoli
+                var atlagok = reactor.HomersekletLog.Skip(1) //első elem az alap elem
+                           .Select((value, index) => new { Value = value, Group = index / 6 })
+                           .GroupBy(x => x.Group)
+                           .Select(group => group.Average(x => x.Value));
+                Console.WriteLine($"A reaktor kezdőhőmérséklete: {reactor.HomersekletLog[0]}");
+                Console.WriteLine("Minden negyed nap átlaghőmérséklete:");
+                foreach (var item in atlagok) Console.WriteLine(item);
+                var minmax = reactor.HomersekletLog.Skip(1)
+                                 .Select((value, index) => new { Value = value, Group = index / 6 })
+                                 .GroupBy(x => x.Group)
+                                 .Select(group => new
+                                 {
+                                     Minimum = group.Min(x => x.Value),
+                                     Maximum = group.Max(x => x.Value)
+                                 });
+                Console.WriteLine("Minden 6 elem minimuma és maximuma (0 kihagyva):");
+                foreach (var item in minmax) Console.WriteLine($"Minimum: {item.Minimum}, Maximum: {item.Maximum}");
+
+
             }
             catch (Exception ex) {
                 Console.WriteLine($"Hiba történt: {ex.Message}");
             }
             Console.ReadKey();
-            /*delegált használata
+            /*
             • eseménykezelés(legalább egy eseményé)
             • a mérési adatok elhelyezése adatbázisban
             • a mérési adatok kiírása JSON fájlba
