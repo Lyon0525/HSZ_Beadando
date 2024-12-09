69828d61bdd586344164e8da019f89c9f8c1ce73
Lyon0525 <palfikristof2004.kfpl@gmail.com>
2024-12-09 17:26:58 +0100

    Random szammal feltoltes

--- Program.cs
+++ Program.cs
@@ -7,13 +7,22 @@ using HSZ_Beadando_DLL;
 
 namespace HSZ_Beadando {
     internal class Program {
-        static List<Reaktor> reactor = new List<Reaktor>();
+        static Reaktor reactor = new Reaktor(0,0,0);
         static void Main(string[] args) {
             try {
                 Random r = new Random();
+                reactor = new Reaktor(r.Next(5, 11), 0, r.Next(550, 651));
                 for (int i = 0; i < 60; i++) { //1 perc
-
-                    r.NextDouble();
+                    if (i % 3 == 0) {
+                        reactor.VizNyomasLog.Add(0 - r.NextDouble());
+                        reactor.MegtermeltEnergiaLog.Add(0 - r.NextDouble());
+                        reactor.HomersekletLog.Add(0 - r.NextDouble());
+                    }
+                    else {
+                        reactor.VizNyomasLog.Add(r.NextDouble());
+                        reactor.MegtermeltEnergiaLog.Add(r.NextDouble());
+                        reactor.HomersekletLog.Add(r.NextDouble());
+                    }
                 }
             }
             catch (Exception ex) {
