8fbf76e9414d4e72219841a9200bd2ecc6af826f
Lyon0525 <palfikristof2004.kfpl@gmail.com>
2024-12-09 17:14:35 +0100

    Random szammal feltoltes felig kesz

--- Program.cs
+++ Program.cs
@@ -7,9 +7,18 @@ using HSZ_Beadando_DLL;
 
 namespace HSZ_Beadando {
     internal class Program {
+        static List<Reaktor> reactor = new List<Reaktor>();
         static void Main(string[] args) {
+            try {
+                Random r = new Random();
+                for (int i = 0; i < 60; i++) { //1 perc
 
-
+                    r.NextDouble();
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Hiba történt: {ex.Message}");
+            }
             Console.ReadKey();
         }
     }
