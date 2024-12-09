b1a8637b2905e6bce78776958fd9f514b5c6af62
Lyon0525 <palfikristof2004.kfpl@gmail.com>
2024-12-09 17:52:37 +0100

    Random szam generalas done

--- Program.cs
+++ Program.cs
@@ -11,19 +11,22 @@ namespace HSZ_Beadando {
         static void Main(string[] args) {
             try {
                 Random r = new Random();
-                reactor = new Reaktor(r.Next(5, 11), 0, r.Next(550, 651));
-                for (int i = 0; i < 60; i++) { //1 perc
+                reactor = new Reaktor(r.Next(5, 11), r.Next(4, 6), r.Next(550, 651));
+                for (int i = 0; i < 24; i++) { //1 perc
                     if (i % 3 == 0) {
                         reactor.NyomasAllitas(0 - r.NextDouble());
                         reactor.EnergiaValtozas(0 - r.NextDouble());
                         reactor.Homersekletallitas(0 - r.NextDouble());
                     }
                     else {
-                        reactor.NyomasAllitas(r.NextDouble());
-                        reactor.EnergiaValtozas(r.NextDouble());
-                        reactor.Homersekletallitas(r.NextDouble());
+                        reactor.NyomasAllitas(r.NextDouble() / 2);
+                        reactor.EnergiaValtozas(r.NextDouble() / 2);
+                        reactor.Homersekletallitas(r.NextDouble() * 2);
                     }
+                    reactor.Kiiras();
                 }
+                
+
             }
             catch (Exception ex) {
                 Console.WriteLine($"Hiba történt: {ex.Message}");
