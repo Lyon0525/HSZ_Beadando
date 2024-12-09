2f80bac91d4f7a492c67df2a96919945e23bf04f
Lyon0525 <palfikristof2004.kfpl@gmail.com>
2024-12-09 17:42:36 +0100

    Random szamadas megvaltoztatasa

--- Program.cs
+++ Program.cs
@@ -14,14 +14,14 @@ namespace HSZ_Beadando {
                 reactor = new Reaktor(r.Next(5, 11), 0, r.Next(550, 651));
                 for (int i = 0; i < 60; i++) { //1 perc
                     if (i % 3 == 0) {
-                        reactor.VizNyomasLog.Add(0 - r.NextDouble());
-                        reactor.MegtermeltEnergiaLog.Add(0 - r.NextDouble());
-                        reactor.HomersekletLog.Add(0 - r.NextDouble());
+                        reactor.NyomasAllitas(0 - r.NextDouble());
+                        reactor.EnergiaValtozas(0 - r.NextDouble());
+                        reactor.Homersekletallitas(0 - r.NextDouble());
                     }
                     else {
-                        reactor.VizNyomasLog.Add(r.NextDouble());
-                        reactor.MegtermeltEnergiaLog.Add(r.NextDouble());
-                        reactor.HomersekletLog.Add(r.NextDouble());
+                        reactor.NyomasAllitas(r.NextDouble());
+                        reactor.EnergiaValtozas(r.NextDouble());
+                        reactor.Homersekletallitas(r.NextDouble());
                     }
                 }
             }
