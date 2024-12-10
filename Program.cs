6a3b104506f5d568f00288302a7f74ca9b670268
Lyon0525 <palfikristof2004.kfpl@gmail.com>
2024-12-10 19:04:25 +0100

    SQL

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 using HSZ_Beadando_DLL;
 
 namespace HSZ_Beadando {
